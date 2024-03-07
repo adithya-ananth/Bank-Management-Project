e7ccb2f6af7adff558c8e8fe1a6de0f9c7281a94
Adithya <53051177+adithya-ananth@users.noreply.github.com>
2024-03-08 01:11:23 +0530

    Update Admin_Account.java

--- Bank/Admin_Account.java
+++ Bank/Admin_Account.java
@@ -2,19 +2,40 @@ package Bank;
 
 import java.util.*;
 
+@SuppressWarnings("unused")
+
 class Admin {
-    final long phone_num = 9876543210L;
-    final String email = "cs23b001@iittp.ac.in";
-    final String username = "cs23b001";
-    final String password = "Spongebob@123";
+    private final long phone_num = 9876543210L;
+    private final String email = "cs23b069@iittp.ac.in";
+    private final String username = "cs23b069";
+    private final String password = "Spongebob@123";
 
+    // we need to find out why these arraylists must be decalred as 'static'
+    public static ArrayList<User> users_list = new ArrayList<User>();
+    public static ArrayList<String> overall_logs = new ArrayList<String>();
+
+    // need to link the Transaction DB 
     public void viewLogs() {
-        
-    } 
+        for (int i = 0; i < overall_logs.size(); i++)
+            System.out.println(overall_logs.get(i));
+    }
 
-    public void viewUsers(ArrayList<User> accounts_list) {
-        for (int i = 0; i < accounts_list.size(); i++)
-            System.out.printf("User %d:\nUser ID: %s\nFirst Name: %s\nLast Name: %s\nEmail: %s\n\n", i+1, accounts_list.get(i).user_id, accounts_list.get(i).fname, accounts_list.get(i).lname, accounts_list.get(i).email);
+    public void viewUsers() {
+        for (int i = 0; i < users_list.size(); i++)
+            System.out.printf("User %d:\nUser ID: %s\nFirst Name: %s\nLast Name: %s\nEmail: %s\n\n", i+1, users_list.get(i).user_id, users_list.get(i).fname, users_list.get(i).lname, users_list.get(i).email);
+    }
+
+    public boolean isAdmin(String username, String password) {
+        if (!this.username.equals(username)) {
+            System.out.println("Incorrect username");
+            return false;
+        }
+        else if (!this.password.equals(password)) {
+            System.out.println("Incorrect password");
+            return false;
+        }
+
+        return true;
     }
 }
 

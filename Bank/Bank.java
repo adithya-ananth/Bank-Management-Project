cdc27918d932c62b420ff9194739eff6da60a259
Adithya <53051177+adithya-ananth@users.noreply.github.com>
2024-03-14 00:24:03 +0530

    Update Bank.java

--- Bank/Bank.java
+++ Bank/Bank.java
@@ -1,11 +1,11 @@
 package Bank;
 
-import java.util.*;
-
 public class Bank {
     public static void main(String[] args) {
         Admin admin = new Admin();
         admin.isAdmin("cs23b069", "Spongebob@123");
+        
+        // Dry running admin features
         
         // System.out.println(admin.phone_num);
         // System.out.println(admin.email);
@@ -13,5 +13,12 @@ public class Bank {
         // System.out.println(admin.password);
 
         admin.viewUsers();
+
+        // Dry running user and savingsAccount declaration and methods
+        
+        // User u1 = new User("323946346397", "Ashwin", "Ravichandran", "rashwin123@gmail.com", "9848817722", 1234, 1900000000);
+        // System.out.println(u1.login(1234));
+        // savingsAccount s1 = new savingsAccount("323946346397", "Ashwin", "Ravichandran", "rashwin123@gmail.com", "9848817722", 1234, 1900000000, 5);
+        // System.out.println(s1.amount + s1.accountType + s1.interestRate);
     }
 }

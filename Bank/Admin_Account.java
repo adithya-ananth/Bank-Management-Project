da56333b2f1b0f83303ee42ab200d3bdae9b1d2e
Adithya <53051177+adithya-ananth@users.noreply.github.com>
2024-03-14 00:23:48 +0530

    Update Admin_Account.java

--- Bank/Admin_Account.java
+++ Bank/Admin_Account.java
@@ -5,24 +5,24 @@ import java.util.*;
 @SuppressWarnings("unused")
 
 class Admin {
-    private final long phone_num = 9876543210L;
+    private final long phoneNum = 9876543210L;
     private final String email = "cs23b069@iittp.ac.in";
     private final String username = "cs23b069";
     private final String password = "Spongebob@123";
 
     // we need to find out why these arraylists must be decalred as 'static'
-    public static ArrayList<User> users_list = new ArrayList<User>();
-    public static ArrayList<String> overall_logs = new ArrayList<String>();
+    public static ArrayList<User> usersList = new ArrayList<User>();
+    public static ArrayList<String> overallLogs = new ArrayList<String>();
 
     // need to link the Transaction DB 
     public void viewLogs() {
-        for (int i = 0; i < overall_logs.size(); i++)
-            System.out.println(overall_logs.get(i));
+        for (int i = 0; i < overallLogs.size(); i++)
+            System.out.println(overallLogs.get(i));
     }
 
     public void viewUsers() {
-        for (int i = 0; i < users_list.size(); i++)
-            System.out.printf("User %d:\nUser ID: %s\nFirst Name: %s\nLast Name: %s\nEmail: %s\n\n", i+1, users_list.get(i).user_id, users_list.get(i).fname, users_list.get(i).lname, users_list.get(i).email);
+        for (int i = 0; i < usersList.size(); i++)
+            System.out.printf("User %d:\nUser ID: %s\nFirst Name: %s\nLast Name: %s\nEmail: %s\n\n", i+1, usersList.get(i).userId, usersList.get(i).fname, usersList.get(i).lname, usersList.get(i).email);
     }
 
     public boolean isAdmin(String username, String password) {
